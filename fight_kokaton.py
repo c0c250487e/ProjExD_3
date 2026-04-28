11f88291a5b4bef4efb14c97e1bf9ad2dd72046e
c0c250487e <c0c250487e@edu.teu.ac.jp>
2026-04-28 14:40:05 +0900

    練習３よろこびエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,9 @@ def main():
                 if beam.rct.colliderect(bomb.rct):  # 練習2：爆弾とビームの衝突判定
                     beam = None
                     bomb = None
+                    bird.change_img(6, screen)  # 練習3：こうかとん喜びエフェクト
+                    pg.display.update()
+                    time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
